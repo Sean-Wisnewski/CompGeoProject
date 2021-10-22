071cfc38a6a59583b7185603af179449f2b62a1a
savagewil <willsvg@gmail.com>
2021-10-22 13:30:05 -0400

    Added the algorithmn?

--- helpers.py
+++ helpers.py
@@ -4,11 +4,15 @@ from heapq import heappush, heappop, heappushpop, heapify
 from functools import total_ordering
 from matplotlib import pyplot as plt
 
+from visualizations import show_polygon
+
+
 @total_ordering
 class Point:
     """
     Class to represent a 2d point in space
     """
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -21,6 +25,7 @@ class Point:
 
     def __lt__(self, pt2):
         return self.x <= pt2.x
+
 
 class StackElement:
     def __init__(self, pt, chain):
@@ -45,6 +50,7 @@ class LineSegment:
     def __eq__(self, e2):
         return self.pt0 == e2.pt0 and self.pt1 == e2.pt1
 
+
 def compute_interior_angle(pt0, pt1, pt2):
     """
     Computes the interior angle of a vertex given 3 points.
@@ -54,12 +60,13 @@ def compute_interior_angle(pt0, pt1, pt2):
     :param pt2:
     :return:
     """
-    angle = math.atan2(pt1.y - pt0.y, pt1.x-pt0.x) - math.atan2(pt2.y - pt0.y, pt2.x - pt0.x)
+    angle = math.atan2(pt1.y - pt0.y, pt1.x - pt0.x) - math.atan2(pt2.y - pt0.y, pt2.x - pt0.x)
     if angle < 0:
-        angle += 2*math.pi
-    elif angle > 2*math.pi:
-        angle -= 2*math.pi
+        angle += 2 * math.pi
+    elif angle > 2 * math.pi:
+        angle -= 2 * math.pi
     return angle
+
 
 def read_input_to_pts_list(fname):
     pts = []
@@ -71,12 +78,14 @@ def read_input_to_pts_list(fname):
             pts.append(Point(float(vals[0]), float(vals[1])))
     return pts
 
+
 def check_monotonicity(pts, dir='x'):
     if dir == 'x':
-        for idx in range(len(pts)-1):
-            if pts[idx] >= pts[idx+1]:
+        for idx in range(len(pts) - 1):
+            if pts[idx] >= pts[idx + 1]:
                 return False
     return True
+
 
 def split_to_chains(pts, as_one_list=False):
     """
@@ -94,12 +103,9 @@ def split_to_chains(pts, as_one_list=False):
     upper_as_list = []
     lower_as_list = []
     for idx, pt in enumerate(pts):
-        if max_idx < idx < min_idx:
+        if max_idx <= idx < min_idx:
             heappush(lower, pt)
-        elif max_idx > idx or idx > min_idx:
-            heappush(upper, pt)
         else:
-            heappush(lower, pt)
             heappush(upper, pt)
     if not as_one_list:
         while len(upper) > 0:
@@ -122,6 +128,7 @@ def split_to_chains(pts, as_one_list=False):
                 all_pts.append(StackElement(heappop(lower), "lower"))
         return all_pts
 
+
 def check_vertex_reflexive(vi, vi_1, vi_2):
     angle = compute_interior_angle(vi_1, vi, vi_2)
     if angle >= math.pi:
@@ -129,81 +136,107 @@ def check_vertex_reflexive(vi, vi_1, vi_2):
     else:
         return False
 
+
 def orient_test(p, q, r):
     return (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
 
+
+def visible(pts, j, u):
+    for i in range(j + 1, u):
+        if pts[i].chain == "lower" and orient_test(pts[j].pt, pts[i].pt, pts[u].pt) >= 0:
+            return False
+        elif pts[i].chain == "upper" and orient_test(pts[j].pt, pts[i].pt, pts[u].pt) <= 0:
+            return False
+    return True
+
+
 # whereas I could implement merge to merge the two queues, I'm kinda lazy
-def x_monotone_triangulation(pts : list):
-    stack = [pts.pop(), pts.pop()]
-    u = stack[0]
+def x_monotone_triangulation(pts: list):
+    stack = []
+    stack.insert(0, 0)
+    stack.insert(0, 1)
+    # u = stack[0]
     diagonals = []
-    while len(pts) > 0:
-        vi = pts.pop()
-        vi_last = stack[-1]
-        # case 1: vi and vi-1 on opposite chains:
-        if vi.chain != vi_last.chain:
-            for idx in range(len(stack)-1, -1, -1):
-                if stack[idx] != u:
-                    diagonals.append(LineSegment(stack[idx], vi))
-            u = vi_last
-        # case 2: vi on same chain as vi-1
+
+    for j in range(2, len(pts) - 2):
+        if pts[stack[0]].chain != pts[j].chain:
+            for u in stack[:-1]:
+                diagonals.append(LineSegment(pts[u].pt, pts[j].pt))
+            stack.insert(0, j - 1)
+            stack.insert(0, j)
         else:
-            # i.e. vi-1 is a non-reflex vertex
-            if not check_vertex_reflexive(vi.pt, vi_last.pt, stack[-2].pt):
-                # add diagonals until no more vertices can be seen from vi
-                if vi.chain == "lower":
-                    visible = True
-                    can_continue = True
-                    while visible and can_continue:
-                        q = stack[-1]
-                        r = stack[-2]
-                        #Todo fix
-                        q_visible = orient_test(vi.pt, q.pt, r.pt) < 0
-                        if q_visible:
-                            diagonals.append(LineSegment(vi.pt, q.pt))
-                            can_continue = orient_test(vi.pt, r.pt, q.pt) > 0
-                            stack.pop()
-                else:
-                    visible = True
-                    can_continue = True
-                    while visible and can_continue:
-                        q = stack[-1]
-                        r = stack[-2]
-                        #Todo fix
-                        q_visible = orient_test(vi.pt, q.pt, r.pt) > 0
-                        if q_visible:
-                            diagonals.append(LineSegment(vi.pt, q.pt))
-                            can_continue = orient_test(vi.pt, r.pt, q.pt) < 0
-                            stack.pop()
-            # vi-1 is a reflex vertex, so just add vi to the stack
-            else:
-                stack.append(vi)
+            u = u_l = stack.pop(0)
+            while visible(pts, j, u):
+                diagonals.append(LineSegment(pts[u].pt, pts[j].pt))
+                u = stack.pop(0)
+            stack.insert(0, u_l)
+            stack.insert(0, j)
+    for u in stack[1:-1]:
+        diagonals.append(LineSegment(pts[u].pt, pts[-1].pt))
     return diagonals
+    # while len(pts) > 1:
+    #     vi = pts.pop()
+    #     vi_last = stack[-1]
+    #     # case 1: vi and vi-1 on opposite chains:
+    #     if vi.chain != vi_last.chain:
+    #         for idx in range(len(stack)-1, -1, -1):
+    #             if stack[idx] != u:
+    #                 diagonals.append(LineSegment(stack[idx], vi))
+    #         u = vi_last
+    #     # case 2: vi on same chain as vi-1
+    #     else:
+    #         # i.e. vi-1 is a non-reflex vertex
+    #         if not check_vertex_reflexive(vi.pt, vi_last.pt, stack[-2].pt):
+    #             # add diagonals until no more vertices can be seen from vi
+    #             if vi.chain == "lower":
+    #                 visible = True
+    #                 can_continue = True
+    #                 while visible and can_continue:
+    #                     q = stack[-1]
+    #                     r = stack[-2]
+    #                     #Todo fix
+    #                     q_visible = orient_test(vi.pt, q.pt, r.pt) < 0
+    #                     if q_visible:
+    #                         diagonals.append(LineSegment(vi.pt, q.pt))
+    #                         can_continue = orient_test(vi.pt, r.pt, q.pt) > 0
+    #                         stack.pop()
+    #             else:
+    #                 visible = True
+    #                 can_continue = True
+    #                 while visible and can_continue:
+    #                     q = stack[-1]
+    #                     r = stack[-2]
+    #                     #Todo fix
+    #                     q_visible = orient_test(vi.pt, q.pt, r.pt) > 0
+    #                     if q_visible:
+    #                         diagonals.append(LineSegment(vi.pt, q.pt))
+    #                         can_continue = orient_test(vi.pt, r.pt, q.pt) < 0
+    #                         stack.pop()
+    #         # vi-1 is a reflex vertex, so just add vi to the stack
+    #         else:
+    #             stack.append(vi)
+    # return diagonals
 
 
 pts = (read_input_to_pts_list("inputs/simple_all_positive.txt"))
-X = [pt.x for pt in pts + [pts[0]]]
-Y = [pt.y for pt in pts + [pts[0]]]
-plt.plot(X, Y)
-plt.show()
+
+show_polygon(pts)
+
 upper, lower = split_to_chains(pts)
 
-upper_X = [pt.x for pt in upper]
-upper_Y = [pt.y for pt in upper]
-plt.plot(upper_X, upper_Y)
-plt.show()
+show_polygon(upper, False)
 
+show_polygon(lower, False)
 
-lower_X = [pt.x for pt in lower]
-lower_Y = [pt.y for pt in lower]
-plt.plot(lower_X, lower_Y)
-plt.show()
+all_pts = split_to_chains(pts, True)
 
-# all_pts = split_to_chains(pts, True)
-# print(check_monotonicity(pts))
-# print(check_monotonicity([pt.pt for pt in all_pts]))
-# all_pts = (list(reversed(all_pts)))
-# diags = x_monotone_triangulation(all_pts)
-# print(diags)
+show_polygon([pt.pt for pt in all_pts], False)
+
+all_pts = (list(reversed(all_pts)))
+print(all_pts)
+
+print(check_monotonicity(pts))
+diags = x_monotone_triangulation(all_pts)
+print(diags)
 # #print(check_monotonicity(upper) and check_monotonicity(lower))
 #
--- visualizations.py
+++ visualizations.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def show_polygon(pts):
+def show_polygon(pts, end_connection=True):
     xs = [pt.x for pt in pts]
     ys = [pt.y for pt in pts]
     fig, ax = plt.subplots()
@@ -9,5 +9,6 @@ def show_polygon(pts):
         if idx < len(pts)-1:
             pt2 = pts[idx+1]
             ax.plot([pt.x, pt2.x], [pt.y, pt2.y], color='r')
+    if end_connection:
         plt.plot([pts[0].x, pts[-1].x], [pts[0].y, pts[-1].y], color='r')
     plt.show()
